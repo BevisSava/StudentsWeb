faee6100f9456c7bc859a91a19152eae5d0d518e
ADMIN <ADMIN@LAPTOP-B46IMTI2>
2026-04-22 02:12:20 +0700

    Add project files.

---
+++ StudentsWeb/Controllers/ReportController.cs
@@ -0,0 +1,47 @@
+﻿using StudentsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Data.Entity.Infrastructure.Design.Executor;
+
+namespace StudentsWeb.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["MyRpt"].ConnectionString;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GenerateReport(DateTime fromDate, DateTime toDate)
+        {
+            List<Student> students = new List<Student>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Student WHERE BirthDate BETWEEN @FromDate AND @ToDate";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@FromDate", fromDate);
+                cmd.Parameters.AddWithValue("@ToDate", toDate);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    students.Add(new Student
+                    {
+                        Id = (int)reader["Id"],
+                        Name = (string)reader["Name"],
+                        BirthDate = (DateTime)reader["BirthDate"]
+
+                    });
+                }
+            }
+            return View("ReportView", students);
+        }
+    }
+}
---
+++ StudentsWeb/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWeb.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
